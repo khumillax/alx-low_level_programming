e3e16a018e1ed6eaa02e450d1632fd0eda0f7eec
khumillax <vuyosn.khumalo@gmail.com>
2023-02-21 17:41:06 +0000

    unified diff2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,20 +4,20 @@
 /**
  * main - entry point
  *
- * return: always 0 
+ * Return: always 0
  */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	
 	if (n > 0)
 	{
 	printf("%d is positive\n", n);
-	}	
+	}
 	if (n == 0)
 	{
 	printf("%d is zero\n", n);
