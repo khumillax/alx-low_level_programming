8ccbef0641e131845d68729171c24531b3a0c86e
khumillax <vuyosn.khumalo@gmail.com>
2023-03-15 17:50:44 +0000

    hexidecimal and compiled

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main- print all the numbers for base 16. 0-9 a-f. 
+ *
+ *Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
