d25f074966a5ddc0bae3ad9f3a8a2abccc35ac41
khumillax <vuyosn.khumalo@gmail.com>
2023-03-15 17:34:31 +0000

    task 5 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - pritn single digits of base 10 from 0
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+
+	return(0);
+}
