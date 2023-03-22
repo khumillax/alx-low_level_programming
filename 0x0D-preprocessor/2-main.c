750caf93d7720a77c54f6b5ca67eedebc201e8dc
khumillax <vuyosn.khumalo@gmail.com>
2023-03-22 20:27:22 +0000

    preproessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints name of the file
+ *Description: prints name of file
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
