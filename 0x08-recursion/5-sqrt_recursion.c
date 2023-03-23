beb2fe160114bb92e2d7b4c9969b63778c7278a1
khumillax <vuyosn.khumalo@gmail.com>
2023-03-23 15:00:58 +0000

    trailing space

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,7 +17,7 @@ int _sqrt_recursion(int n)
 /**
  * actual_sqrt_recursion - recurses to find the natural
  * square root of a number
- * @n: number to calculate the square root 
+ * @n: number to calculate the square root
  * @i: iterator
  * Return: the resulting square root
  */
